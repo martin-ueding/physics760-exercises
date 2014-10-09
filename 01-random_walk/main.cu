acdc9b4fbfd3dd1458f1dd6de48ec8e15eb109b5
Martin Ueding <dev@martin-ueding.de>
2014-10-09 20:05:41 +0200

    Write some code

--- 01-random_walk/main.cu
+++ 01-random_walk/main.cu
@@ -3,7 +3,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void handle_error(cudaError_t err, int line) {
+    if (err != cudaSuccess) {
+        printf("%s at line %d\n", cudaGetErrorString(err), line);
+        exit(EXIT_FAILURE);
+    }
+}
+
+__global__
+void reset_kernel(struct walker *walkers_dev, int walker_count, float *distances_dev) {
+}
+
+__global__
+void random_walk_kernel(struct walker *walkers_dev, int walker_count, float *distances_dev) {
+}
+
+struct walker {
+    int x;
+    int y;
+};
+
 int main(int argc, char **argv) {
+    int walker_count = 10000;
+    cudaError_t err;
+
+    // Get memory to store positions in.
+    struct walker *walkers_dev;
+    err = cudaMalloc(&walkers_dev, sizeof(*walkers_dev) * walker_count);
+    handle_error(err, __LINE__);
+
+    // Get memory to store distance in.
+    float *distances_dev;
+    err = cudaMalloc(&distances_dev, sizeof(*distances_dev) * walker_count);
+    handle_error(err, __LINE__);
+
 
     return 0;
 }
